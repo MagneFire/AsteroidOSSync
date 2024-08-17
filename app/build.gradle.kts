b74390ca9b80e07889d67ddeade761b114556158
Darrel Griët <dgriet@gmail.com>
2024-08-17 19:30:02 +0200

    Update Nordic BLE lib
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,6 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.osmdroid:osmdroid-android:6.1.16")
     implementation("no.nordicsemi.android.support.v18:scanner:1.6.0")
-    implementation("no.nordicsemi.android:ble:2.7.2")
+    implementation("no.nordicsemi.android:ble:2.8.0")
     implementation("com.google.guava:guava:33.1.0-android")
 }
